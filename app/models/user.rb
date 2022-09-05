0d5ca2b1e7ff532163c7372f4df0b793cc9ce7b7
EC2 Default User <ec2-user@ip-172-31-35-65.ap-northeast-3.compute.internal>
2022-09-05 14:49:26 +0000

    continue[bookers2]

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
    has_many :books,dependent: :destroy
+   
+  def index
+  end
          
   def show
   end
